659d6721c0b10a68b3cda103b66c78bedfdf2cab
Natthakit <nonusae@gmail.com>
2017-10-27 02:43:29 +0700

    Intregrate first spec test for notification create

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -4,8 +4,18 @@ require 'rails_helper'
 RSpec.describe Notification, type: :model do
   describe 'creation' do
     it 'can be created' do
-      notification = Notification.create(phone: "555-555-5555", body: "My message", source_app: "some_app")
+      notification = FactoryGirl.build_stubbed(:notification)
       expect(notification).to be_valid
     end
+  end
+
+  describe 'validations' do
+    it 'can be created if valid' do
+      notification = FactoryGirl.build_stubbed(:notification)
+      notification.phone = nil
+      notification.body = nil
+      notification.source_app = nil
+      expect(notification).to_not be_valid
+    end  
   end
 end
